63faab0c2208cd2db8c8447619a4f433ea4ea20a
evanl105 <evantluong105@gmail.com>
2026-01-28 15:04:00 -0800

    Added All Needed Materials

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Didn't use this file for the lab because I included the script directly in the HTML file.
+// I did that because that was how I was learning throughout the map so I felt more comfortable doing it that way.
+// However, if I were to put code here, this is where it would go.
+
